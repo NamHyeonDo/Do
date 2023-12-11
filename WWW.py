a20df49123df47bd499a8bf26b2da6f0dafaa811
NamHyeonDo <ska03wjd@gmail.com>
2023-12-11 17:15:06 +0900

    HH

--- WWW.py
+++ WWW.py
@@ -1,10 +1,183 @@
 import FinanceDataReader as fdr
 import matplotlib.pyplot as plt
-import seaborn as
+import seaborn as sns
 import 
 
 
-tips = sns.load_dataset("tips")
+""" tips = sns.load_dataset("tips")
 
 sns.regplot(x="total_bill", y="tip", data=tips)
 plt.show()
+
+sns.barplot(x="day", y="total_bill", data=tips, palette="viridis")
+plt.title("Average Tips") """
+""" 
+titanic = sns.load_dataset("titanic")
+
+sns.barplot(x="sex", y="survived", hue="pclass", data=titanic)
+
+# 탑승클래스별 인원구성
+sns.countplot(x="class", hue="who", data=titanic)
+
+# 클래스별 생존자
+sns.countplot(x="class", hue="alive", data=titanic)
+
+# 성별별 생존자
+sns.countplot(x="sex", hue="alive", data=titanic)
+
+# 싱글 여행자별 인원구성
+sns.countplot(x="alone", hue="who", data=titanic)
+
+# 탑승지별 생존자의 클래스 구별
+sns.barplot(x="embark_town", y="survived", hue="pclass", data=titanic)
+ """
+""" import FinanceDataReader as fdr
+
+df = fdr.DataReader("KS11")
+df0 = df.loc["2023"]
+
+df0["Close"].plot()
+
+plt.grid(True)
+
+dow = fdr.DataReader("DJI", "2010-06-01")
+kospi = fdr.DataReader("KS11", "2010-06-01")
+
+# 각 지수별 종가 기준 그래프 출력
+plt.plot(dow.index, dow.Close, "r--", label="Dow")
+plt.plot(kospi.index, kospi.Close, "b", label="KOSPI") """
+
+""" # 해당 데이터를 가져오는 URL
+# code 는 종목명
+url = "https://finance.naver.com/item/sise_day.nhn?code={}"
+
+res = requests.get(url, headers=headers)
+html = bs(res.text, "html.parser")
+html_table = html.select("table")
+table = pd.read_html(str(html_table))
+
+page = 1
+for page in range(1, 100):
+    page_url = f"{url}&page={page}"
+    print(page_url)
+
+    response = requests.get(page_url, headers=headers)
+    html = bs(response.text, "html.parser")
+    html_table = html.select("table")
+    table = pd.read_html(str(html_table))
+
+    if not table[0].empty:
+        df_list.append(table[0].dropna())
+        page += 1
+    else:
+        break
+
+df = pd.concat(df_list, ignore_index=True)
+
+df = df.dropna()
+df = df.iloc[0:30]
+df = df.sort_values(by="날짜")
+
+plt.plot(df["날짜"], df["종가"], "co-") """
+
+""" #MS
+ticker = "MSFT"
+
+# Apple
+ticker = "APPL"
+start_date = "2022-01-01"
+end_date = "2023-12-02"
+
+data = yf.download(ticker, start=start_date, end=end_date)
+
+plt.plot(data["Close"], label="Close Price")
+
+# 50일 평균
+data["MA_50"] = data["Close"].rolling(window=50).mean()
+
+# 200일 평균
+data["MA_200"] = data["Close"].rolling(window=200).mean() """
+
+url = "https://www.worldometers.info/world-population/population-by-country/"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+for row in rows:
+    columns = row.select("td")
+    country = columns[1].get_text(strip=True)
+    population = int(columns[2].get_text(strip=True).replace(",", ""))
+    
+    countries.append(country)
+    populations.append(population)
+    
+    plt.barh(top_countries[::-1], top_populations[::-1], color="skyblue")
+plt.xlabel("Population")
+plt.title("Top 10")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
